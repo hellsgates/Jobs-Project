7bd438b280697f1eddea544b8653dfdf882f6c69
Timofey <whatisflopz@gmail.com>
2025-02-07 13:20:16 +0300

    add delete and update options

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -4,9 +4,13 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg"> {{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg"> {{ $job->title }}</h2>
     <p>
-        This job pays {{ $job['salary'] }} per year.
+        This job pays {{ $job->salary }} per year.
     </p>
+
+            <x-button class="mt-6" href="/jobs/{{ $job->id }}/edit">
+                Edit Job
+            </x-button>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
     dd($jobs[0]);
 });
 
+// index
 Route::get('/jobs', function ()  {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -18,16 +19,19 @@ Route::get('/jobs', function ()  {
     ]);
 });
 
+// create
 Route::get('/jobs/create', function ()  {
     return view('jobs.create');
 });
 
+// show one job
 Route::get('/jobs/{id}', function ($id)  {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// store
 Route::post('/jobs', function ()  {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -43,6 +47,39 @@ Route::post('/jobs', function ()  {
     return redirect('/jobs');
 });
 
+Route::get('/jobs/{id}/edit', function ($id)  {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// update
+Route::patch('/jobs/{id}', function ($id)  {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    // todo authorize
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// delete
+Route::delete('/jobs/{id}', function ($id)  {
+    // todo authorize
+
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+});
 
 Route::get('/about', function () {
     return view('about');
